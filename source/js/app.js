4228e8d2bf605ab6c2c2e75a61cb0181458f380a
Alexandr Dementev <feralist21@gmail.com>
2022-09-09 12:35:15 +0800

    Добавил кейсы

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,8 @@
-window.onload = function(){
-	console.log('Готов!');
+window.onload = function () {
+  console.log('Готов!');
   toggleMobileMenu();
   introSlider();
+  casesSlider();
 };
 
 function toggleMobileMenu() {
@@ -9,19 +10,33 @@ function toggleMobileMenu() {
   const mobileMenuWrap = document.querySelector('.js-menu');
 
   if (toggleBtn && mobileMenuWrap) {
-    toggleBtn.addEventListener('click', function() {
+    toggleBtn.addEventListener('click', function () {
       mobileMenuWrap.classList.toggle('header-mobile__menu-wrap--active');
     });
   }
 }
 
 function introSlider() {
-  const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper('.intro__slider', {
     loop: true,
     autoHeight: true,
-    autoplay: {
-      delay: 5000,
-    },
-    pauseOnMouseEnter: true,
+    // autoplay: {
+    //   delay: 5000,
+    // },
+    // pauseOnMouseEnter: true,
   });
+}
+
+function casesSlider() {
+  if (window.innerWidth <= 768) {
+    const swiper = new Swiper('.cases-slider', {
+      loop: false,
+      spaceBetween: 20,
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.cases-pagination',
+      },
+    });
+  };
+
 }
